f19a91ff27258ff87894c9c0229a0bca8c657e3f
Martin Etmajer <martin.etmajer@boehringer-ingelheim.com>
2020-03-17 20:03:01 +0100

    Move execution of pipeline stages into a single job

---
+++ vars/mroPipeline.groovy
@@ -0,0 +1,53 @@
+def call(Map config) {
+
+    def project = [:]
+    def repos   = []
+
+    def debug = config.get('debug', false)
+
+    node {
+        checkout scm
+
+        withEnv (mroEnvironment(debug)) {
+            stage('Init') {
+                echo "**** STARTING stage Init ****"
+                def result = phaseInit()
+                project = result.project
+                repos = result.repos
+                echo "**** ENDED stage Init ****"
+            }
+
+            stage('Build') {
+                echo "**** STARTING stage Build ****"
+                phaseBuild(project, repos)
+                echo "**** ENDED stage Build ****"
+            }
+
+            stage('Deploy') {
+                echo "**** STARTING stage Deploy ****"
+                phaseDeploy(project, repos)
+                echo "**** ENDED stage Deploy ****"
+            }
+
+            stage('Test') {
+                echo "**** STARTING stage Test ****"
+                phaseTest(project, repos)
+                echo "**** ENDED stage Test ****"
+            }
+
+            stage('Release') {
+                echo "**** STARTING stage Release ****"
+                phaseRelease(project, repos)
+                echo "**** ENDED stage Release ****"
+            }
+
+            stage('Finalize') {
+                echo "**** STARTING stage Finalize ****"
+                phaseFinalize(project, repos)
+                echo "**** ENDED stage Finalize ****"
+            }
+        }
+    }
+}
+
+return this
