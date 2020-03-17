e588e3496cf0515c86da2120b3315e98c4779daf
Martin Etmajer <martin.etmajer@boehringer-ingelheim.com>
2020-03-17 11:39:34 +0100

    Remove D from environments that produce bugs

--- src/org/ods/usecase/JiraUseCase.groovy
+++ src/org/ods/usecase/JiraUseCase.groovy
@@ -226,7 +226,7 @@ class JiraUseCase {
         }
 
         this.support.applyXunitTestResults(testIssues, testResults)
-        if (["D", "Q", "P"].contains(this.project.buildParams.targetEnvironmentToken)) {
+        if (["Q", "P"].contains(this.project.buildParams.targetEnvironmentToken)) {
             // Create bugs for erroneous test issues
             def errors = JUnitParser.Helper.getErrors(testResults)
             this.createBugsForFailedTestIssues(testIssues, errors, this.steps.env.RUN_DISPLAY_URL)
