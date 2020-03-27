d65431bc6bb9a7eeae234012cc68d3d7b5c1fb80
clemensutschig <clemens.utschig-utschig@boehringer-ingelheim.com>
2020-03-27 17:03:50 +0100

    find slave directory

--- src/org/ods/service/LeVADocumentChaptersFileService.groovy
+++ src/org/ods/service/LeVADocumentChaptersFileService.groovy
@@ -25,6 +25,7 @@ class LeVADocumentChaptersFileService {
         def file = Paths.get(this.steps.env.WORKSPACE, DOCUMENT_CHAPTERS_BASE_DIR, "${documentType}.yaml").toFile()
         if (!file.exists()) {
             this.steps.sh("find . -name ${documentType}.yaml")
+            this.steps.sh("pwd")
             throw new RuntimeException("Error: unable to load document chapters. File '${file.toString()}' does not exist.")
         } else {
             this.steps.echo("Found file for ${documentType} @ '${file.toString()}'")
