52367ec9b20e9b32f42dbd1f7c38f66b5976ed6a
clemensutschig <clemens.utschig-utschig@boehringer-ingelheim.com>
2020-04-07 11:27:22 +0200

    debug and add uppercase

--- src/org/ods/util/Project.groovy
+++ src/org/ods/util/Project.groovy
@@ -1068,7 +1068,8 @@ class Project {
           "ods.build.rm.${getKey()}.repo.commit.timestamp=${gitData.time}",
           "ods.build.rm.${getKey()}.repo.commit.author=${gitData.author}",
           "ods.build.rm.${getKey()}.repo.branch=${gitData.baseTag}",
-          "ods.build.mro.lib.version=${mroSharedLibVersion}"
+          "ods.build.mro.lib.version=${mroSharedLibVersion}",
+          "ODS.TEST=clemens"
         ]
     }
 }
